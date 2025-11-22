9ae897b64e0fbcf1efc876fa46f01689df054f07
Gabriel Yasuhiro Zoppello Kanashiro <gyasuhir@c3r8p2.42sp.org.br>
2025-11-22 17:30:56 -0300

    add ex01

---
+++ ex01/include/Contact.hpp
@@ -0,0 +1,19 @@
+#include <string>
+
+class Contact {
+	private:
+		std::string first_name_;
+		std::string last_name_;
+		std::string surname_;
+		std::string phone_number_;
+		std::string darkest_secret_;
+	public:
+		void createContact();
+		void displayContact();
+		void displayHeader();
+};
+
+void createContact()
+{
+	return ;
+}
